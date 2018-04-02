f018d610b2d3e984697872a060478b7944d0eb31
Ken Sible <kjs0001@mix.wvu.edu>
2018-04-02 18:38:33 -0400

    Added LoginInterface

--- src/parsecsei/SEIClient.java
+++ src/parsecsei/SEIClient.java
@@ -15,7 +15,7 @@ public class SEIClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new LoginInterface().setVisible(true);
     }
     
 }
